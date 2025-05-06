56cdd08c3cc853a7785551957dcc39bef056504c
Feorj Miah <01ferojmiah@gmail.com>
2025-05-06 22:59:24 +0600

    Slide change for demo purpose

--- Course Project/Form-App/obj/Release/net8.0/Form-App.AssemblyInfo.cs
+++ Course Project/Form-App/obj/Release/net8.0/Form-App.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Form-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da08dfce25f4e369ec51ebdf8e0bba13b265e6bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b5035fd552b85da45693ec02803aa5974fa3c8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Form-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Form-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
